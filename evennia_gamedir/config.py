1e9213654ff50bebcab62b9ce930fb436300f867
Greg Taylor <snagglepants@gmail.com>
2016-04-08 16:00:19 -0700

    Detect whether we're runnign in production or not.

---
+++ evennia_gamedir/config.py
@@ -0,0 +1,15 @@
+"""
+Functions for loading and setting configuration on the Flask app.
+"""
+import os
+
+
+def populate_flask_config(app):
+    """
+    :param flask.app.Flask app: The Flask app to configure.
+    """
+    server_software = os.environ.get('SERVER_SOFTWARE', '')
+    if not server_software or server_software.startswith('Development'):
+        app.config['IS_PRODUCTION'] = False
+    else:
+        app.config['IS_PRODUCTION'] = True
