dd7ca616d1a3e0374ef029bbbc81997d557d1411
Nicholas Matlaga <nmatlaga@outlawtack.com>
2017-10-27 11:51:11 -0400

    added checks for bin/checkin.py for Evennia 0.6

--- bin/checkin.py
+++ bin/checkin.py
@@ -6,7 +6,7 @@ import platform
 
 url = 'http://127.0.0.1:8080/api/v1/game/check_in'
 values = {
-    'game_name': 'Test Güame',
+    'game_name': 'Test Güame Evennia 0.7',
     'game_status': 'pre-alpha',
     'game_website': 'http://blah.com',
     'listing_contact': 'blah@blah.com',
@@ -23,16 +23,33 @@ values = {
     'telnet_port': '1234',
     'web_client_url': 'http://webclient.blah.com',
 
-    'connected_player_count': 10,
-    'total_player_count': 123,
+    'connected_account_count': 10,
+    'total_account_count': 123,
 
-    'evennia_version': '0.5.0',
+    'evennia_version': '0.7.0',
     'python_version': platform.python_version(),
     'django_version': '1.8.x',
     'server_platform': platform.platform(),
 }
 data = urllib.urlencode(values)
 req = urllib2.Request(url, data)
+print("Testing Evennia 0.7 format request")
+try:
+    response = urllib2.urlopen(req)
+except urllib2.HTTPError as exc:
+    print exc.read()
+else:
+    result = response.read()
+    print result
+
+del values['connected_account_count']
+del values['total_account_count']
+values['game_name'] = "Test Güame Evennia 0.6"
+values['total_player_count'] = 123
+values['connected_player_count'] = 10
+data = urllib.urlencode(values)
+req = urllib2.Request(url, data)
+print("Testing Evennia 0.6 format request")
 try:
     response = urllib2.urlopen(req)
 except urllib2.HTTPError as exc:
